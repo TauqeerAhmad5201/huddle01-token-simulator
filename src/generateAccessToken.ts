aca0ed84edb2c1f8cbeaad327944adcac1c30e2c
vrajdesai78 <vrajdesai78@gmail.com>
2023-12-18 20:34:39 +0530

    Create token simulator

---
+++ src/generateAccessToken.ts
@@ -0,0 +1,27 @@
+"use server";
+import { AccessToken, Role } from "@huddle01/server-sdk/auth";
+
+export const generateAccessToken = async (roomId: string) => {
+  const accessToken = new AccessToken({
+    apiKey: process.env.API_KEY!,
+    roomId: roomId,
+    role: Role.HOST,
+    permissions: {
+      admin: true,
+      canConsume: true,
+      canProduce: true,
+      canProduceSources: {
+        cam: true,
+        mic: true,
+        screen: true,
+      },
+      canRecvData: true,
+      canSendData: true,
+      canUpdateMetadata: true,
+    },
+  });
+
+  const token = await accessToken.toJwt();
+
+  return token;
+};
